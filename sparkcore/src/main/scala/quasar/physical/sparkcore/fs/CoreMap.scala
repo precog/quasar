294a0257f6eb81b3fe54b0309c61035b68873db7
Pawel Szulc <paul.szulc@gmail.com>
2016-12-09 17:46:37 +0100

    Allow compareing Timestamp & Date in queries

--- sparkcore/src/main/scala/quasar/physical/sparkcore/fs/CoreMap.scala
+++ sparkcore/src/main/scala/quasar/physical/sparkcore/fs/CoreMap.scala
@@ -374,6 +374,8 @@ object CoreMap extends Serializable {
     case (Data.Interval(a), Data.Interval(b)) => Data.Bool(a.compareTo(b) < 0)
     case (Data.Str(a), Data.Str(b)) => Data.Bool(a.compareTo(b) < 0)
     case (Data.Timestamp(a), Data.Timestamp(b)) => Data.Bool(a.compareTo(b) < 0)
+    case (Data.Timestamp(a), Data.Date(b)) =>
+      fromDateTime(a)(zdt => Data.Bool(zdt.toLocalDate().compareTo(b) < 0))
     case (Data.Date(a), Data.Date(b)) => Data.Bool(a.compareTo(b) < 0)
     case (Data.Time(a), Data.Time(b)) => Data.Bool(a.compareTo(b) < 0)
     case (Data.Bool(a), Data.Bool(b)) => (a, b) match {
@@ -391,6 +393,8 @@ object CoreMap extends Serializable {
     case (Data.Interval(a), Data.Interval(b)) => Data.Bool(a.compareTo(b) <= 0)
     case (Data.Str(a), Data.Str(b)) => Data.Bool(a.compareTo(b) <= 0)
     case (Data.Timestamp(a), Data.Timestamp(b)) => Data.Bool(a.compareTo(b) <= 0)
+    case (Data.Timestamp(a), Data.Date(b)) =>
+      fromDateTime(a)(zdt => Data.Bool(zdt.toLocalDate().compareTo(b) <= 0))
     case (Data.Date(a), Data.Date(b)) => Data.Bool(a.compareTo(b) <= 0)
     case (Data.Time(a), Data.Time(b)) => Data.Bool(a.compareTo(b) <= 0)
     case (Data.Bool(a), Data.Bool(b)) => (a, b) match {
@@ -408,6 +412,8 @@ object CoreMap extends Serializable {
     case (Data.Interval(a), Data.Interval(b)) => Data.Bool(a.compareTo(b) > 0)
     case (Data.Str(a), Data.Str(b)) => Data.Bool(a.compareTo(b) > 0)
     case (Data.Timestamp(a), Data.Timestamp(b)) => Data.Bool(a.compareTo(b) > 0)
+    case (Data.Timestamp(a), Data.Date(b)) =>
+      fromDateTime(a)(zdt => Data.Bool(zdt.toLocalDate().compareTo(b) > 0))
     case (Data.Date(a), Data.Date(b)) => Data.Bool(a.compareTo(b) > 0)
     case (Data.Time(a), Data.Time(b)) => Data.Bool(a.compareTo(b) > 0)
     case (Data.Bool(a), Data.Bool(b)) => (a, b) match {
@@ -425,6 +431,8 @@ object CoreMap extends Serializable {
     case (Data.Interval(a), Data.Interval(b)) => Data.Bool(a.compareTo(b) >= 0)
     case (Data.Str(a), Data.Str(b)) => Data.Bool(a.compareTo(b) >= 0)
     case (Data.Timestamp(a), Data.Timestamp(b)) => Data.Bool(a.compareTo(b) >= 0)
+    case (Data.Timestamp(a), Data.Date(b)) =>
+      fromDateTime(a)(zdt => Data.Bool(zdt.toLocalDate().compareTo(b) >= 0))
     case (Data.Date(a), Data.Date(b)) => Data.Bool(a.compareTo(b) >= 0)
     case (Data.Time(a), Data.Time(b)) => Data.Bool(a.compareTo(b) >= 0)
     case (Data.Bool(a), Data.Bool(b)) => (a, b) match {

510ca2a88b73980426dd0902733a3d6083a2632c
Scala Steward <me@scala-steward.org>
2020-01-25 01:06:22 +0100

    Update refined, refined-scalacheck, ... to 0.9.12

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,7 +11,7 @@ object Versions {
   val matryoshkaVersion   = "0.18.3"
   val monocleVersion      = "1.6.0"
   val pathyVersion        = "0.2.13"
-  val refinedVersion      = "0.9.10"
+  val refinedVersion      = "0.9.12"
   val scodecBitsVersion   = "1.1.12"
   val scalacheckVersion   = "1.14.3"
   val scalazVersion       = "7.2.30"
